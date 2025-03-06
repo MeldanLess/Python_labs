6568bd80f412ce764c8e75331b117119e474c1bf
MeldanLess <danila.meleca@gmail.com>
2025-03-06 14:03:35 +0300

    Задание 2 Вариант 4

---
+++ Lab4/Zadanie2Variant14/Zadanie2Variant14.py
@@ -0,0 +1,10 @@
+import re
+
+def solve(f):
+    with open(f, encoding='utf-8') as file:
+        txt = file.read().lower()
+        lst = re.findall(r'\b[а-яё]+\b', txt)
+        res = sum(1 for w in lst if len(w) > 1 and w[0] == w[-1])
+    return res
+
+print(solve("/mnt/data/text.txt"))
